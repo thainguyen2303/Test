dbbf9d67a22ab448419a7b22f4d937caf20c5d71
thai.nguyen <thai.nguyen@filestring.com>
2013-12-23 16:17:16 +0700

    update2

--- src/com/example/testgit/MainActivity.java
+++ src/com/example/testgit/MainActivity.java
@@ -11,6 +11,9 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		System.out.println(">>>>>> Test");
+		System.out.println(">>>>>> Test");
+		System.out.println(">>>>>> Test");
+		System.out.println(">>>>>> Test");
 	}
 
 	@Override
